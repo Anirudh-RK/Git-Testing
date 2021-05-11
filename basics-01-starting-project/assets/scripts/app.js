f04b502eeb68feaf94a577086e9dfd6f52f3d830
Anirudh-RK <anirk3468@gmail.com>
2021-05-11 15:04:37 -0400

    First commit for JavaScript.

---
+++ basics-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,10 @@
+const defaultResult = 0;
+
+let currentResult = defaultResult;
+
+currentResult = currentResult + 10;
+
+let desc = `( ${defaultResult} + 10)`;
+
+outputResult(currentResult, desc);
+
