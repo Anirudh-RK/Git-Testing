ff38a6b0b4e77d976a9ccd856353cc0951583906
Anirudh-RK <anirk3468@gmail.com>
2021-05-17 21:56:13 -0400

    changes to file - including arrays

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -2,6 +2,8 @@ const defaultResult = 0;
 
 let currentResult = defaultResult;
 
+let logEntry = [];
+
 function getNum() {
     return parseInt(userInput.value);
 }
@@ -11,12 +13,24 @@ function getOutput(operator, prevResult, result){
     outputResult(currentResult, resultDesc);
 }
 
+function writeToLog(operationId, prevResult, operationNum, newResult){
+    const entry = {
+        operation: operationId,
+        num: prevResult,
+        operand: operationNum,
+        result: newResult
+    };
+    logEntry.push(entry);
+    console.log(logEntry);
+    
+}
+
 function add(num1, num2){
     const newNum = getNum();
     const initialNum = currentResult;
     currentResult = currentResult + newNum;
     getOutput('+', initialNum, newNum);
-    
+    writeToLog('Add', initialNum, newNum, currentResult);
 }
 
 function subtract(){
@@ -24,7 +38,7 @@ function subtract(){
     const initialNum = currentResult;
     currentResult = currentResult - newNum;
     getOutput('-', initialNum, newNum);
-
+    writeToLog('Subtract', initialNum, newNum, currentResult);
 }
 
 function multiply(){
@@ -32,6 +46,7 @@ function multiply(){
     const initialNum = currentResult;
     currentResult = currentResult * newNum;
     getOutput('*', initialNum, newNum);
+    writeToLog('Multiply', initialNum, newNum, currentResult);
 }
 
 function divide(){
@@ -39,6 +54,7 @@ function divide(){
     const initialNum = currentResult;
     currentResult = currentResult / newNum;
     getOutput('/', initialNum, newNum);
+    writeToLog('Divide', initialNum, newNum, currentResult);
 }
 
 addBtn.addEventListener('click', add);
